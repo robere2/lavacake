bc7bced68b9253eb2f3fc22e99f27a1fd9a44ded
Erik <7330807+robere2@users.noreply.github.com>
2023-09-24 10:28:20 -0400

    Update hypixelApi.ts to use API_TOKEN env variable

--- src/utils/hypixelApi.ts
+++ src/utils/hypixelApi.ts
@@ -1,16 +1,13 @@
-import tokensJSON from '../../token.json'
-const tokens = tokensJSON as {
-    active: 'devToken' | 'appToken'
-    devToken: string
-    appToken: string
+const token = process.env.API_TOKEN ?? ''
+const uuidv4Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
+if (!token || !uuidv4Regex.test(token)) {
+    throw new Error('Invalid Hypixel API token. Please get a token from https://developer.hypixel.net/ and set it to your API_TOKEN environment variable. Check the README for more information.')
 }
-
-if (!tokens[tokens.active]) console.warn("You didn't make a valid token.json file! Please refer to README.md!")
 
 function get(endpoint: string) {
     return fetch(`https://api.hypixel.net` + endpoint, {
         method: 'GET',
-        headers: { 'API-Key': tokens[tokens.active] },
+        headers: { 'API-Key': token },
     })
 }
 export default { get }
